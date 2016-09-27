87a3082d9665ec86c73765bb057ef3e5d1f8fe5b
Preetham Narayana Reddy <preethamnr1997@gmail.com>
2016-09-27 17:42:38 +0530

    Update randomForest.py

--- Iris_dataset/randomForest.py
+++ Iris_dataset/randomForest.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import train_test_split
 
 dataset = pd.read_csv('Iris.csv')
 y = dataset.iloc[: , 5]
@@ -11,42 +12,23 @@ sc = StandardScaler()
 sc.fit(X)
 X = sc.transform(X)
 
-        #for i in range(len(y)):
-        #    if(y.iloc[i] == 'Iris-setosa'):
-        #        y.iloc[i] = 1
-        #    elif(y.iloc[i] == 'Iris-virginica'):
-        #        y.iloc[i] = 3
-        #    else:
-        #        y.iloc[i] = 2
-
 y = y.replace(to_replace= 'Iris-setosa', value = 1)
 y = y.replace(to_replace= 'Iris-virginica', value = 3)
 y = y.replace(to_replace = 'Iris-versicolor', value = 2)
 y = y.tolist()
 
-        #X = X.as_matrix()
-        #print(type(y))
-        #print(type(X))
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.3)
 
-        #print('1')
 forest = RandomForestClassifier(criterion='entropy', n_estimators=10)
-forest.fit(X, y)
+forest.fit(X_train, y_train)
 
-        #print('2')
-
-predictions = forest.predict(X)
+predictions = forest.predict(X_test)
 total_correct = 0
 
-        #print('3')
-
-for i in range(len(y)):
-    if(predictions[i] == y[i]):
+for i in range(len(y_test)):
+    if(predictions[i] == y_test[i]):
         total_correct = total_correct + 1
 
-        #print('4')
-
-accuracy = total_correct * 1.0/ len(y)
-
-        #print('5')
+accuracy = total_correct * 1.0/ len(y_test)
 
 print (accuracy)
